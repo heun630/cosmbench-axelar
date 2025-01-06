fc3b4976e1843a0a866914bc148e53f47bc7e8d4
heun630 <egg6030@gmail.com>
2025-01-06 13:06:10 +0900

    fix: shell scripts

--- 91_init.sh
+++ 91_init.sh
@@ -53,8 +53,14 @@ do
     
     #sed -i 's/address = \"0.0.0.0:9090\"/address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}\"/g' $CURRENT_DATA_DIR/config/app.toml
     echo "sed -i 's/address = \"0.0.0.0:9090\"/address = "'${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}'"/g' $CURRENT_DATA_DIR/config/app.toml"
+
+    # gRPC port
     sed -i "s/address = \"0.0.0.0:9090\"/address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml
+
+    # gRPC Web
     sed -i "s/address = \"0.0.0.0:9091\"/address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_WEB_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml
+
+    # tcp
     sed -i "s/address = \"tcp:\/\/0.0.0.0:1317\"/address = \"tcp:\/\/${PRIVATE_HOSTS[$INDEX]}:${API_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml
 done
 
