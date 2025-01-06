b19eaaf53a47d739723c591a47213859c9b2b519
heun630 <egg6030@gmail.com>
2025-01-06 11:49:18 +0900

    fix: shell scripts

--- 91_init.sh
+++ 91_init.sh
@@ -53,9 +53,8 @@ do
     
     #sed -i 's/address = \"0.0.0.0:9090\"/address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}\"/g' $CURRENT_DATA_DIR/config/app.toml
     echo "sed -i 's/address = \"0.0.0.0:9090\"/address = "'${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}'"/g' $CURRENT_DATA_DIR/config/app.toml"
-    sed -i "s#address = \"0.0.0.0:9900\"#address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}\"#g" $CURRENT_DATA_DIR/config/app.toml
+    sed -i "s/address = \"0.0.0.0:9090\"/address = \"${PRIVATE_HOSTS[$INDEX]}:${GRPC_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml
 
-    echo "sed -i "s/address = \"tcp:\/\/0.0.0.0:1317\"/address = \"tcp:\/\/${PRIVATE_HOSTS[$INDEX]}:${API_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml"
     sed -i "s/address = \"tcp:\/\/0.0.0.0:1317\"/address = \"tcp:\/\/${PRIVATE_HOSTS[$INDEX]}:${API_PORTS[$INDEX]}\"/g" $CURRENT_DATA_DIR/config/app.toml
 done
 
