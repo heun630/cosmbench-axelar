46562951a81a5fc927ae89ff2087ad8e84e84983
heun630 <egg6030@gmail.com>
2025-01-06 10:39:29 +0900

    fix: shell scripts

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 CHAIN_NAME="axelar"
 CHAIN_ID=$CHAIN_NAME"-cosmbench" #블록체인 ID
-BINARY="./bin/"$CHAIN_NAME"d"
+BINARY="/usr/local/bin/"$CHAIN_NAME"d"
 MONIKER="cosmbench"
 # ADDRESS_PREFIX="mssong"
 
