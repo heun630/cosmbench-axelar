b9cd3fffd87f0dbf20f4729d37e53fe7832e6703
heun630 <egg6030@gmail.com>
2025-01-15 16:18:00 +0900

    feat: test data

--- scripts/1_init_nodes.sh
+++ scripts/1_init_nodes.sh
@@ -1,23 +1,22 @@
-# init node해서 default genesis.json 만드는 스크립트
-
 #!/bin/bash
-source ./env.sh
+
+# Load environment variables relative to the scripts directory
+SCRIPT_DIR=$(dirname "$0")
+source "$SCRIPT_DIR/env.sh"
 
 rm -rf $NODE_ROOT_DIR
 
-echo "The number of nodes: "$NODE_COUNT
-for ((i = 0; i < $NODE_COUNT; i++))
-do
+echo "The number of nodes: $NODE_COUNT"
+for ((i=0; i<NODE_COUNT; i++)); do
     CURRENT_DATA_DIR=$NODE_ROOT_DIR/node$i
     $BINARY init $MONIKER$i --chain-id $CHAIN_ID --home $CURRENT_DATA_DIR
-    
+
     cp -f $CURRENT_DATA_DIR/config/genesis.json $CURRENT_DATA_DIR/config/sample_genesis.json
 done
-echo "The number of nodes: "$NODE_COUNT
+echo "The number of nodes: $NODE_COUNT"
 
 echo "[ SHOW NODE ID ]"
-for ((i = 0; i < $NODE_COUNT; i++))
-do
+for ((i=0; i<NODE_COUNT; i++)); do
     CURRENT_DATA_DIR=$NODE_ROOT_DIR/node$i
     $BINARY tendermint show-node-id --home $CURRENT_DATA_DIR
 done
