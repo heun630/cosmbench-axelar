8693d7390b3ab19146d51ff42f7a935e2ea8f020
heun630 <egg6030@gmail.com>
2025-01-21 15:29:13 +0900

    fix: run script

--- scripts/92_run.sh
+++ scripts/92_run.sh
@@ -10,17 +10,17 @@ INDEX=$1
 CURRENT_DATA_DIR=$TESTDIR/node$INDEX
 
 # Set log directory and log file
-#LOG_DIR="logs"
-#LOG_FILE="$LOG_DIR/output$INDEX.log"
+LOG_DIR="logs"
+LOG_FILE="$LOG_DIR/output$INDEX.log"
 
 ## Create logs directory if it does not exist
-#mkdir -p "$LOG_DIR"
+mkdir -p "$LOG_DIR"
 #
 ## Print the command that will be run (for debugging purposes)
 echo "$BINARY start --home $CURRENT_DATA_DIR"
 #
 ## Remove existing log file
-#rm -f "$LOG_FILE"
+rm -f "$LOG_FILE"
 
 $BINARY start --home "$CURRENT_DATA_DIR" 2>&1 | while IFS= read -r line; do
   if [[ "$line" == *"committed state"* ]]; then
